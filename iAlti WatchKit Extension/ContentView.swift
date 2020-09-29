23405ba82b4962080d8ac41d5d550ad17d88b099
Luke Crouch <lukaswheldon@yahoo.de>
2020-09-29 19:33:24 +0200

    Initial Commit

---
+++ iAlti WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iAlti WatchKit Extension
+//
+//  Created by Lukas Wheldon on 29.09.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iAlti WatchKit Extension/iAltiApp.swift
@@ -0,0 +1,19 @@
+//
+//  iAltiApp.swift
+//  iAlti WatchKit Extension
+//
+//  Created by Lukas Wheldon on 29.09.20.
+//
+
+import SwiftUI
+
+@main
+struct iAltiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
